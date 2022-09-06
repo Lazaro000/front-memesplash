9ba6e11a61ba14a2a99981e6971a60c2dd6b9098
Lázaro <lazaroyaesta@gmail.com>
2022-09-06 19:41:20 +0200

    ✨ Button component

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Button from '../components/atoms/button';
+
+const Home = () => {
+	return (
+		<div>
+			<h1>Home</h1>
+			<Button kind='primary' size='regular'>
+				Registrate
+			</Button>
+		</div>
+	);
+};
+
+export default Home;
