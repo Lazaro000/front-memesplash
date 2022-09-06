e62a370d15e5765db21d5f97760bbec1b75bfb32
Lázaro <lazaroyaesta@gmail.com>
2022-09-06 20:01:31 +0200

    ✨ Basic login with localstorage

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react';
+import { AuthContext } from '../contexts/auth-context';
 import '../styles/index.css';
 
-const MyApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+	const [auth, setAuth] = useState();
+
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		if (token) setAuth(token);
+	}, []);
+
+	return (
+		<AuthContext.Provider value={{ auth, setAuth }}>
+			<header></header>
+			<Component {...pageProps} />
+			<footer></footer>
+		</AuthContext.Provider>
+	);
 };
 
-export default MyApp;
+export default App;
